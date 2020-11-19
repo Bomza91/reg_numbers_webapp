751eb178f6cc93d7a7aa4abe4159d5eb43d74bd9
Bomza91 <bmasentile@gmail.com>
2020-11-19 14:21:37 +0200

    added the route file, linked my route file with injex.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
@@ -7,6 +8,8 @@ const session = require('express-session');
 const pg = require("pg");
 const Pool = pg.Pool;
 const _ = require('lodash');
+const regApp = require("./routes");
+
 
 
 const app = express();
@@ -19,6 +22,7 @@ const pool = new Pool({
 });
 
 const regInstance = regFactory(pool);
+const routeInst = regApp(regInstance)
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main', layoutsDir: 'views/layouts' }));
 app.set('view engine', 'handlebars');
@@ -35,83 +39,17 @@ app.use(session({
 app.use(flash());
 app.use(express.static('public'));
 
-app.get('/', async function (req, res) {
+app.get('/', routeInst.home)
 
+app.post('/reg_numbers', routeInst.post)
 
-    const plates = await regInstance.getReg()
-    res.render('index', {
-        plate: plates
+app.get('/reg_numbers', routeInst.filterBy)
 
-    })
-});
-
-app.post('/reg_numbers', async function (req, res) {
-    var reg = _.upperCase(req.body.regNumberEntered);
-    var duplicate = await regInstance.checkingReg(reg)
-
-    if (!reg) {
-        req.flash('info', 'Please enter your registration number');
-    }
-
-    else if (duplicate !== 0) {
-        req.flash('info', 'registration already exist')
-    }
-
-    else if (!(/C[AKJ]\s\d{3}-\d{3}$|C[AKJ]\s\d{3}$|C[AKJ]\s\d{3} \d{3}$/.test(reg))) {
-        req.flash('error', 'Enter a correct registration number');
-    }
-   
-    else {
-        await regInstance.regNumber(reg)
-        req.flash('success', 'You have entered a correct registration number')
-    }
-
-
-    const plates = await regInstance.getReg()
-    res.render('index', {
-        plate: plates
-
-    })
-
-});
-
-app.get('/reg_numbers', async function (req, res) {
-
-    let area = req.query.town
-    console.log(area);
-
-    if (!area) {
-        req.flash('info', 'Please select a town');
-    } else {
-        const filtering = await regInstance.showFilter(area)
-
-        res.render('index', {
-            plate: filtering
-        })
-    }
-});
-
-app.get('/reset', async function (req, res) {
-
-    await regInstance.reset()
-    res.render('index');
-});
+app.get('/reset', routeInst.clearBtn)
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, function () {
     console.log("App started at port", PORT)
 });
-
-
-
-
-
-
-
-
-``
-
-
-
 
 
--- routes.js
+++ routes.js
@@ -1,12 +1,75 @@
-module.exports = function route(registration){
+module.exports = function route(regInstance){
     var _ = require('lodash');
 
-  
+    async function home(req, res) {
 
+
+        const plates = await regInstance.getReg()
+        res.render('index', {
+            plate: plates
+    
+        })
+    };
+
+    async function post(req, res) {
+        var reg = _.upperCase(req.body.regNumberEntered);
+        var duplicate = await regInstance.checkingReg(reg)
+    
+        if (!reg) {
+            req.flash('info', 'Please enter your registration number');
+        }
+    
+        else if (duplicate !== 0) {
+            req.flash('info', 'registration already exist')
+        }
+    
+        else if (!(/C[AKJ]\s\d{3}-\d{3}$|C[AKJ]\s\d{3}$|C[AKJ]\s\d{3} \d{3}$/.test(reg))) {
+            req.flash('error', 'Enter a correct registration number');
+        }
+       
+        else {
+            await regInstance.regNumber(reg)
+            req.flash('success', 'You have entered a correct registration number')
+        }
+    
+    
+        const plates = await regInstance.getReg()
+        res.render('index', {
+            plate: plates
+    
+        })
+    
+    };
+
+    async function filterBy(req, res) {
+
+        let area = req.query.town
+        console.log(area);
+    
+        if (!area) {
+            req.flash('info', 'Please select a town');
+        } else {
+            const filtering = await regInstance.showFilter(area)
+    
+            res.render('index', {
+                plate: filtering
+            })
+        }
+    };
+
+    async function clearBtn(req, res) {
+
+        await regInstance.reset()
+        res.render('index');
+    };
+    
 
 
     return{
-
+    home, 
+    post,
+    filterBy,
+    clearBtn
 
     }
     
